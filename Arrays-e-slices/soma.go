d2dd0023de425da5cf853d8e66a61479a49a495f
Ana Paula Campos Westphalen <ana.westphalen@enjoei.com.br>
2022-04-30 10:56:40 -0300

    refatoring code and tests about array and slices

--- Arrays-e-slices/soma.go
+++ Arrays-e-slices/soma.go
@@ -9,7 +9,37 @@ func Soma(numeros []int) int {
 	return soma
 }
 
+// func SomaTudo(numerosParaSomar ...[]int) (somas []int) {
+//     quantidadeDeNumeros := len(numerosParaSomar)
+// 	somas = make([]int, quantidadeDeNumeros)
+
+// 	for i, numeros := range numerosParaSomar {
+// 		somas[i] = Soma(numeros)
+// 	}
+
+// 	return
+// }
+
+func SomaTodoOResto(numerosParaSomar ...[]int) []int {
+    var somas []int
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+            somas = append(somas, 0)
+        } else {
+            final := numeros[1:]
+            somas = append(somas, Soma(final))
+        }
+	}
+
+	return somas
+}
+
 // Para receber o valor de um array em uma posição específica, basta usar a sintaxe array[índice].
 // O range permite que você percorra um array. Sempre que é chamado,
 // retorna dois valores: o índice e o valor.
 // o valor do índice foi ignorado usando o _ blank identifier.
+// O make te permite criar um slice com
+// uma capacidade inicial de len de numerosParaSomar que precisamos percorrer.
+// Slices podem ser "fatiados"! A sintaxe usada é slice[inicio:final].
+// Se for omitido o valor de um dos lados dos : ele captura tudo do lado omitido.
+// Aqui, quando usamos numeros[1:], estamos dizendo "pegue da posição 1 até o final".
--- Arrays-e-slices/soma_test.go
+++ Arrays-e-slices/soma_test.go
@@ -1,18 +1,45 @@
 package main
 
 import "testing"
+import "reflect"
 
-func TestSoma(t *testing.T) {
+func TestSomaTodoOResto(t *testing.T) {
 
-    t.Run("coleção de qualquer tamanho", func(t *testing.T) {
-        numeros := []int{1, 2, 3}
-
-        resultado := Soma(numeros)
-        esperado := 6
-
-        if resultado != esperado {
-            t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
+    verificaSomas := func(t *testing.T, resultado, esperado []int) {
+        t.Helper()
+        if !reflect.DeepEqual(resultado, esperado) {
+            t.Errorf("resultado %v, esperado %v", resultado, esperado)
         }
+    }
+
+    t.Run("faz a soma do resto", func(t *testing.T) {
+        resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+        esperado := []int{2, 9}
+        verificaSomas(t, resultado, esperado)
     })
 
+    t.Run("soma slices vazios de forma segura", func(t *testing.T) {
+        resultado := SomaTodoOResto([]int{}, []int{3, 4, 5})
+        esperado := []int{0, 9}
+        verificaSomas(t, resultado, esperado)
+    })
+
+    // func TestSomaTudo(t *testing.T) {
+
+    //     recebido := SomaTudo([]int{1,2}, []int{0,9})
+    //     esperado := []int{3, 9}
+
+    //     if !reflect.DeepEqual(recebido, esperado) {
+    //         t.Errorf("resultado %v esperado %v", recebido, esperado)
+    //     }
+    // }
+
 }
+
+// O Go não te deixa usar operadores de igualdade com slices.
+// É possível escrever uma função que percorre cada slice recebido e esperado e
+// verificar seus valores, mas por praticidade podemos usar o reflect.DeepEqual
+// que é útil para verificar se duas variáveis são iguais.
+// Para usar o reflect.DeepEqual necessário importa o "reflect".
+// É importante saber que o reflect.DeepEqual não tem "segurança de tipos",
+// O código compila mesmo se você tentar comparar um string com um slice, por exemplo.
